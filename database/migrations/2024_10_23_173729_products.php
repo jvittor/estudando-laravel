ad19de95979a921c0b46240d6b28bc2c1b451e47
jvittor <102699222+jvittor@users.noreply.github.com>
2024-10-23 14:46:51 -0300

    :sparkles: feat: Products Migrate

--- database/migrations/2024_10_23_173729_products.php
+++ database/migrations/2024_10_23_173729_products.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal("price");
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('products');
     }
 };
