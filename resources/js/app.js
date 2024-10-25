4a96ebe598ac708f3cf0df517f84185097aec904
jvittor <102699222+jvittor@users.noreply.github.com>
2024-10-25 10:19:23 -0300

    :sparkles: feat: Vue.js para construção do Frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import App from './components/App.vue';
+import Products from './components/products.vue'
+
+const app = createApp();
+app.component('app', App);
+app.component('products', Products);
+
+app.mount('#app');
---
+++ resources/views/products.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+
+@section('content')
+    <h2>App from Products</h2>
+    <Products />
+@endsection
